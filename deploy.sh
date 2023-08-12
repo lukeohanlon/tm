a003c2ef379d6f440a9028d903e9e92d14b05ca7
luke0han <lukeohanlon@outlook.com>
2023-08-12 15:45:41 +0100

    yml

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Run the backend container
-# cd backend
+cd "$SCRIPT_DIR/backend"
 docker stop backend-container || true 
 docker rm backend-container || true
 docker run -d -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a --name backend-container app
 
 # Build and run the frontend
-cd frontend
+cd "$SCRIPT_DIR/frontend"
 npm install
-docker stop frontend-container || true # Stop and remove the existing container if it's running
+docker stop frontend-container || true
 docker rm frontend-container || true
 docker build -t frontend-image .
 docker run -d -p 3001:3001 --name frontend-container frontend-image
