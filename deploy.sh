22602de3cfd0c7272c53853bf63f33853d1f0dd9
luke0han <lukeohanlon@outlook.com>
2023-08-12 03:19:46 +0100

    fl

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,7 @@ docker tag backend-image:latest $DOCKER_USERNAME/backend:latest
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push $DOCKER_USERNAME/frontend:latest
 docker push $DOCKER_USERNAME/backend:latest
+
+# Deploy to your EC2 instance using SSH
+ssh -i "MedTracker.pem" $EC2_USERNAME@$EC2_PUBLIC_DNS "cd tm/backend && docker run --rm -it -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a app"
+ssh -i "MedTracker.pem" $EC2_USERNAME@$EC2_PUBLIC_DNS "cd tm/frontend && docker run -d -p 80:80 lukeohanlon/frontend:latest"
