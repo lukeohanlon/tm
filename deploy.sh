5e22341bab0de9c65f98230707116e2eddce52e4
luke0han <lukeohanlon@outlook.com>
2023-08-12 15:37:02 +0100

    yml

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Run the backend container
-cd backend
+# cd backend
 docker run -d -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a --name backend-container app
 
 # Build and run the frontend
