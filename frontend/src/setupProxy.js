193d3f00459d3c8e1ec6291d728a47f57255876f
luke0han <lukeohanlon@outlook.com>
2023-08-12 01:48:04 +0100

    fl

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,8 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware('/api/v1/medications', {
-      target: 'http://16.171.42.210:3000', 
+      target: 'http://16.171.42.210:3000',
       changeOrigin: true,
+      secure: false,
     })
   );
 };
