82d7f5f7d8f40510c54b0f1d97179ddba4470d8a
luke0han <lukeohanlon@outlook.com>
2023-08-12 16:07:23 +0100

    yml

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run the backend container
 cd "$SCRIPT_DIR/backend"
-if docker ps -a --format '{{.Names}}' | grep -q '^backend-container$'; then
-  docker stop backend-container || true
-  docker rm backend-container || true
-fi
+docker stop backend-container || true 
+docker rm backend-container || true
 docker run -d -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a --name backend-container app
 
 # Build and run the frontend
