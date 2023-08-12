d1fcf0d6f20b9739ab1c06fe9d4a6781f5f04d9b
luke0han <lukeohanlon@outlook.com>
2023-08-12 04:46:55 +0100

    fl

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@ ssh -i "MedTracker.pem" $EC2_USERNAME@$EC2_PUBLIC_DNS "
   docker pull lukeohanlon/backend:latest
 
   cd backend # Switch to the backend directory
-  docker run --rm -it -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a lukeohanlon/backend:latest &
-"
+  docker run --rm -it -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a lukeohanlon/backend:latest &"
 
 cd frontend # Switch back to the frontend directory
 npm install
