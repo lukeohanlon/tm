92c695f8047f62800d9675f921637158e16d9dc5
luke0han <lukeohanlon@outlook.com>
2023-08-15 06:19:35 +0100

    serve

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run the backend container
 cd "$SCRIPT_DIR/backend"
-docker stop backend-container || true 
+docker stop backend-container || true
 docker rm backend-container || true
 docker run -d -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a --name backend-container app
 
-# Build the frontend
-cd "$SCRIPT_DIR/frontend"
-npm install
-npm run build
-
-npx http-server -p 3001
+sudo systemctl reload nginx
