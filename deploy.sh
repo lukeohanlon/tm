e7da2bbff2bf3fd39dee7ab33373dd5cc9ad609e
luke0han <lukeohanlon@outlook.com>
2023-08-15 17:31:07 +0100

    dep

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run the backend container
 cd "$SCRIPT_DIR/backend"
-docker stop backend-container || true 
-docker rm backend-container || true
+docker build -t app .
 docker run -d -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a app
 
-# Build and run the frontend
 cd "$SCRIPT_DIR/frontend"
 npm install
-npm start -- --port 3001
+npm run build
+
+# Copy the built assets to the Nginx web directory (usually /var/www/html)
+cp -R build/* /var/www/html/
