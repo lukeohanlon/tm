c730712eedf1f9ebd0d729c1ead940cdd1dca7d5
luke0han <lukeohanlon@outlook.com>
2023-08-15 16:29:04 +0100

    push

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/backend"
 docker stop backend-container || true 
 docker rm backend-container || true
-docker run -d -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a --name backend-container app
+docker run -d -v app-storage:/rails/storage -p 3000:3000 --env RAILS_MASTER_KEY=5e45f0cf35578dc9627cc6fd23e6954a app
 
 # Build and run the frontend
 cd "$SCRIPT_DIR/frontend"
